47ccc8227c7d7139e4f91b3383ed7b63cf20c62b
JacobJones6154 <68295733+JacobJones6154@users.noreply.github.com>
2020-10-28 11:00:02 -0700

    Updated details page

---
+++ Models/TankCompareView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace Tankop.Models
+{
+    public class TankCompareView
+    {
+        public List<Tanks> Tanks { get; set; }
+        public SelectList Class { get; set; }
+        public SelectList Tier { get; set; }
+        public SelectList Country { get; set; }
+        public string TankCountry { get; set; }
+        public string TankClass { get; set; }
+        public string TankTier { get; set; }
+        public string SearchString { get; set; }
+    }
+}
